3c95ba75a6387ecb91424c4a1da2658db9089b9e
Soumya Singh <soumya.25scs1003002659@iilm.edu>
2026-03-25 20:53:49 +0530

    Add files via upload

---
+++ app (1) (1).py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+
+def recommend(movie):
+        movies_index = movies[movies['title'] == movie].index[0]
+        distances = similarity[movies_index]
+        movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+        recommended_movies = []
+        for i in movies_list:
+            recommended_movies.append(movies.iloc[i[0]].title)
+        return recommended_movies
+
+
+movie_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movie_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+st.title('Movie Recommender System')
+
+selected_movie_name = st.selectbox(
+    'Search Your Movie',
+    movies['title'].values)
+
+if st.button('Recommend'):
+   recommendation = recommend(selected_movie_name)
+   for i in recommendation:
+    st.write(i)
